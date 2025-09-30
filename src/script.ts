e321fa9b4dd10ad9da9de239b47d7eb427875e99
Bashar Eid <bashar.eid@viam.com>
2025-09-30 13:23:16 -0400

    refactor

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,19 @@
 import { UserManager } from "oidc-client-ts";
 
-import { CONFIG } from "./code/auth/config";
+import type { OidcClientSettings } from "oidc-client-ts";
+
+const config: OidcClientSettings = {
+  authority: import.meta.env.VITE_AUTHORITY,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+  post_logout_redirect_uri: import.meta.env.POST_LOGOUT_REDIRECT_URI,
+  response_type: "code",
+  scope: "openid profile email",
+  // code_challenge_method: "S256",
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const userManager = new UserManager(CONFIG);
+  const userManager = new UserManager(config);
 
   const urlSearchParams = new URLSearchParams(window.location.search);
 
