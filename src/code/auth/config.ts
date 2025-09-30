fae92187469911a1103ba145ece4db2043d03105
Bashar Eid <beesho.eid@gmail.com>
2025-09-29 23:18:49 -0400

    use correct environment variable

--- src/code/auth/config.ts
+++ src/code/auth/config.ts
@@ -3,7 +3,7 @@ import type { OidcClientSettings } from "oidc-client-ts";
 export const CONFIG: OidcClientSettings = {
   authority: import.meta.env.VITE_AUTHORITY,
   client_id: import.meta.env.VITE_CLIENT_ID,
-  redirect_uri: import.meta.env.REDIRECT_URI,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
   post_logout_redirect_uri: import.meta.env.POST_LOGOUT_REDIRECT_URI,
   response_type: "code",
   scope: "openid profile email",
